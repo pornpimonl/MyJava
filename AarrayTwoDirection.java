9105e3cea8c934dbb12a47bb86938b78ae241003
pornpimon1987 <aweqaweq@hotmail.com>
2021-10-08 14:06:44 +0700

    arrayTwoDirection

---
+++ AarrayTwoDirection.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AarrayTwoDirection {
+    public static void main(String[] args) throws IOException{
+        int row = 4;
+        int col = 2;
+        BufferedReader Stdin = new BufferedReader(new InputStreamReader(System.in));
+        int array[][] = new int[row][col];
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+                System.out.print("array["+i+","+j+"] = ");
+                array[i][j]=Integer.parseInt(Stdin.readLine());
+            }
+        }
+        int total = 0;
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+                total += array[i][j];
+            }
+        }
+        System.out.println("Total = "+total);
+    }
+}
