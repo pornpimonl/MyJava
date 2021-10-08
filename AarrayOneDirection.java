976be0f40eda1d8285589ec920b7158d01c388e8
pornpimon1987 <aweqaweq@hotmail.com>
2021-10-08 13:39:32 +0700

    arrayOneDirection

---
+++ AarrayOneDirection.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * AarrayOneDirection
+ */
+public class AarrayOneDirection {
+    public static void main(String[] args) throws IOException{
+        BufferedReader Stdin = new BufferedReader(new InputStreamReader(System.in));
+        int array[] = new int[4];
+        for(int i=0;i<array.length;i++){
+            System.out.print("array["+i+"] = ");
+            array[i] = Integer.parseInt(Stdin.readLine());
+        }
+        int total = 0;
+        for(int i=0;i<array.length;i++){
+            total += array[i];
+        }
+        System.out.println("Total = "+total);
+    }
+}
